abbc4939948f1316624d635b5fcc5c5f33cb47e6
DarkAndBlue <13067258+DarkAndBlue@users.noreply.github.com>
2021-05-09 16:10:33 +0200

    1.9-1.11 compability for InventoryClickDelayAnalyzer

--- src/main/java/de/jpx3/intave/detect/checks/other/inventoryclickanalysis/InventoryClickDelayAnalyzer.java
+++ src/main/java/de/jpx3/intave/detect/checks/other/inventoryclickanalysis/InventoryClickDelayAnalyzer.java
@@ -11,11 +11,14 @@ import de.jpx3.intave.event.packet.PacketDescriptor;
 import de.jpx3.intave.event.packet.PacketSubscription;
 import de.jpx3.intave.event.packet.Sender;
 import de.jpx3.intave.event.service.violation.Violation;
+import de.jpx3.intave.reflect.ReflectiveAccess;
 import de.jpx3.intave.tools.AccessHelper;
 import de.jpx3.intave.tools.MathHelper;
 import de.jpx3.intave.tools.RotationMathHelper;
+import de.jpx3.intave.tools.annotate.KeepEnumInternalNames;
 import de.jpx3.intave.user.User;
 import de.jpx3.intave.user.UserCustomCheckMeta;
+import de.jpx3.intave.user.UserMetaClientData;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -25,12 +28,16 @@ import java.util.List;
 
 public final class InventoryClickDelayAnalyzer extends IntaveMetaCheckPart<InventoryClickAnalysis, InventoryClickDelayAnalyzer.ClickDelayMeta> {
   private final IntavePlugin plugin;
-  private final boolean invalidVersion;
+  private final boolean newWindowClickVersion;
+  Class<?> clickType;
 
   public InventoryClickDelayAnalyzer(InventoryClickAnalysis parentCheck) {
     super(parentCheck, ClickDelayMeta.class);
-    invalidVersion = ProtocolLibraryAdapter.serverVersion().isAtLeast(MinecraftVersions.VER1_9_0);
+    newWindowClickVersion = ProtocolLibraryAdapter.serverVersion().isAtLeast(MinecraftVersions.VER1_9_0);
     plugin = IntavePlugin.singletonInstance();
+    if (newWindowClickVersion) {
+      clickType = ReflectiveAccess.lookupServerClass("InventoryClickType");
+    }
   }
 
   @PacketSubscription(
@@ -41,82 +48,98 @@ public final class InventoryClickDelayAnalyzer extends IntaveMetaCheckPart<Inven
   )
   public void windowClickPacket(PacketEvent event) {
     Player player = event.getPlayer();
-    if (player.getGameMode().equals(GameMode.CREATIVE) || invalidVersion) {
+    if (player.getGameMode().equals(GameMode.CREATIVE)) {
       return;
     }
-
+    if (ProtocolLibraryAdapter.serverVersion().isAtLeast(MinecraftVersions.VER1_13_0)) {
+      return;
+    }
     User user = userOf(player);
     ClickDelayMeta meta = metaOf(user);
 
-    int slot = event.getPacket().getIntegers().read(1);
-    ItemStack itemStack = event.getPacket().getItemModifier().read(0);
-    int clickedItemID = itemStack.getData().getItemTypeId() * 16 + itemStack.getData().getData();
-
-//    for(Object object : event.getPacket().getModifier().getValues()) {
+//    for (Object object : event.getPacket().getModifier().getValues()) {
 //      String s;
-//      if(object == null) {
+//      if (object == null) {
 //        s = "null";
 //      } else {
 //        s = "" + object + " " + object.getClass();
 //      }
-//
 //      player.sendMessage(s);
 //    }
-    boolean droppedAnItem = event.getPacket().getIntegers().read(3) == 4;
+
+    if (user.meta().clientData().protocolVersion() >= UserMetaClientData.PROTOCOL_VERSION_COLOR_UPDATE) {
+      // TODO: when a player shifts an item in 1.12+ he sends a "null" as itemStack which makes the check imcompatible
+      return;
+    }
+
+    int slot = event.getPacket().getIntegers().read(1);
+    ItemStack itemStack = event.getPacket().getItemModifier().read(0);
+    int clickedItemID = itemStack.getData().getItemTypeId() * 16 + itemStack.getData().getData();
+    boolean droppedAnItem;
+    if (newWindowClickVersion) {
+      InventoryClickTypes clickTypes = event.getPacket().getEnumModifier(InventoryClickTypes.class, clickType).read(0);
+      droppedAnItem = clickTypes == InventoryClickTypes.THROW && slot != -999;
+    } else {
+      droppedAnItem = event.getPacket().getIntegers().read(3) == 4 && slot != -999;
+    }
 
     if (slot != -999 && meta.lastClickedSlot != -999) {
-//      player.sendMessage("" + droppedAnItem + " " + clickedItemID + " " + meta.lastClickedItemID);
       if ((clickedItemID != meta.lastClickedItemID || droppedAnItem) && meta.lastClickedTimeStamp != 0) {
-        double distance = distanceBetween(slot, meta.lastClickedSlot);
-        double time = (System.nanoTime() - meta.lastClickedTimeStamp) / 1000000000d;
-        double speedAttr = distance / time;
+        checkWindowClick(player, meta, slot);
+      }
+    }
 
-        if (time < 2) {
-          meta.clickDelayList.add(time);
-        }
+    prepareNextTick(user, slot, clickedItemID);
+  }
 
-        if (meta.clickDelayList.size() > 15) {
-          double std = RotationMathHelper.calculateStandardDeviation(meta.clickDelayList) * 100;
-//          player.sendMessage("sdt " + new BigDecimal(std).setScale(6, RoundingMode.HALF_UP).toPlainString());
+  private void checkWindowClick(Player player, ClickDelayMeta meta, int slot) {
+    double distance = distanceBetween(slot, meta.lastClickedSlot);
+    double time = (System.nanoTime() - meta.lastClickedTimeStamp) / 1000000000d;
+    double speedAttr = distance / time;
 
-          if (std < 2) {
-//            Violation violation = Violation.builderFor(InventoryClickAnalysis.class)
-//              .withPlayer(player).withDefaultThreshold()
-//              .withMessage("is switching too even between item slots")
-//              .withDetails("moved between slots " + std)
-//              .withVL(time > 0.01 ? 10 : 5).build();
+    if (time < 2) {
+      meta.clickDelayList.add(time);
+    }
+
+    if (meta.clickDelayList.size() > 15) {
+      double std = RotationMathHelper.calculateStandardDeviation(meta.clickDelayList) * 100;
+//      player.sendMessage("sdt " + new BigDecimal(std).setScale(6, RoundingMode.HALF_UP).toPlainString());
+
+      if (std < 2) {
+//        Violation violation = Violation.builderFor(InventoryClickAnalysis.class)
+//          .withPlayer(player).withDefaultThreshold()
+//          .withMessage("is switching too even between item slots")
+//          .withDetails("moved between slots " + std)
+//          .withVL(time > 0.01 ? 10 : 5).build();
 //
-//            //ViolationContext violationContext =
-//            plugin.violationProcessor().processViolation(violation);
-          }
-          meta.clickDelayList.clear();
-        }
+//        //ViolationContext violationContext =
+//        plugin.violationProcessor().processViolation(violation);
+      }
+      meta.clickDelayList.clear();
+    }
 
-        boolean flag = speedAttr > 30;
-        boolean flag2 = speedAttr > 100;
+    boolean flag = speedAttr > 30;
+    boolean flag2 = speedAttr > 100;
 
-        if (distance > 2 && flag && (flag2 || AccessHelper.now() - meta.lastFlagTimeStamp < 5000)) {
-          Violation violation = Violation.builderFor(InventoryClickAnalysis.class)
-            .withPlayer(player).withDefaultThreshold()
-            .withMessage("is switching too quickly between item slots")
-            .withDetails("moved from slot " + meta.lastClickedSlot + " to slot " + slot + " in " + MathHelper.formatDouble(time, 3) + " seconds")
-            .withVL(time > 0.01 ? 10 : 5).build();
+    if (distance > 2 && flag && (flag2 || AccessHelper.now() - meta.lastFlagTimeStamp < 5000)) {
+      Violation violation = Violation.builderFor(InventoryClickAnalysis.class)
+        .withPlayer(player).withDefaultThreshold()
+        .withMessage("is switching too quickly between item slots")
+        .withDetails("moved from slot " + meta.lastClickedSlot + " to slot " + slot + " in " + MathHelper.formatDouble(time, 3) + " seconds")
+        .withVL(time > 0.01 ? 10 : 5).build();
 
-          //ViolationContext violationContext =
-          plugin.violationProcessor().processViolation(violation);
+      //ViolationContext violationContext =
+      plugin.violationProcessor().processViolation(violation);
 //          if (IntaveControl.GOMME_MODE) {
 //            if (distance > 0 && violationContext.violationLevelAfter() > 30) {
 //              userOf(player).applyAttackNerfer(AttackNerfStrategy.DMG_MEDIUM);
 //            }
 //          }
-        }
-
-        if (flag) {
-          meta.lastFlagTimeStamp = AccessHelper.now();
-        }
-      }
     }
-    prepareNextTick(user, slot, clickedItemID);
+
+    if (flag) {
+      meta.lastFlagTimeStamp = AccessHelper.now();
+    }
   }
 
   private void prepareNextTick(User user, int slot, int itemID) {
@@ -144,5 +167,16 @@ public final class InventoryClickDelayAnalyzer extends IntaveMetaCheckPart<Inven
     private long lastClickedTimeStamp;
     private int lastClickedItemID;
     private long lastFlagTimeStamp;
+  }
+
+  @KeepEnumInternalNames
+  public enum InventoryClickTypes {
+    PICKUP,
+    QUICK_MOVE,
+    SWAP,
+    CLONE,
+    THROW,
+    QUICK_CRAFT,
+    PICKUP_ALL;
   }
 }
